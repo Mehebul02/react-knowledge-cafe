7ec74bb598fdf5d9d0b45914f0b7a6c6e0064acf
Mehebul02 <mehebul2123@gmail.com>
2024-03-12 22:57:57 +0600

    Fille setup complite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-6xl bg-red-900 font-bold'>React Knowledge Cafe</h1>
+    
+      
+    </>
+  )
+}
+
+export default App
