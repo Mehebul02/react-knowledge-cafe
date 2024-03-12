9670ae312842d5fa8409a546818f3a281eb7931b
Mehebul02 <mehebul2123@gmail.com>
2024-03-12 23:51:03 +0600

    font family

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@
 const Header = () => {
     return (
         <div>
-                  <h1 className='text-6xl font-bold'>React Knowledge Cafe</h1>
+                  <h1 className='text-6xl font-bold'> Knowledge Cafe</h1>
 
         </div>
     );
