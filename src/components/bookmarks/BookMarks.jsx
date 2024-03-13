50a7981dba07e0ccbf92e26ea88982a780b48aa8
Mehebul02 <mehebul2123@gmail.com>
2024-03-13 12:46:23 +0600

    Read Time commpilted

--- src/components/bookmarks/BookMarks.jsx
+++ src/components/bookmarks/BookMarks.jsx
@@ -2,17 +2,25 @@
 import PropTypes from 'prop-types';
 import Bookmark from '../bookmark/Bookmark';
 const BookMarks = ({bookmarks,readTime}) => {
-    return (
-        
-             <div className='md:w-1/3 text-2xl text-center bg-slate-200 mt-10 font-bold p-4 rounded-lg border-b-2 '>
-                <h1>Reading Time {readTime}</h1>
-            <h1 className='border-b-2 border-blue-800'>Bookmarked Blogs :{bookmarks.length}</h1>
-            
-            {
-                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
-            }
-
-        </div>
+    return (    
+          
+                 <div className='md:w-1/3 text-2xl text-center bg-slate-200 mt-10 font-bold p-4 rounded-lg border-b-2 '>
+              
+             <div> 
+                <h1 className='text-2xl text-blue-800'>Read Time : {readTime}</h1>
+             <h1 className='border-b-2 border-blue-800'>Bookmarked Blogs :{bookmarks.length}</h1>
+             </div>
+              
+              {
+                  bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+              }
+  
+         
+             
+              </div>
+                    
+                
+           
         
     );
 };
